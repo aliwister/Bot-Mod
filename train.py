36b954419bfeacc54727dbf16d4afe5a595b0be1
EC2 Default User <ec2-user@ip-172-31-0-242.us-east-2.compute.internal>
2026-05-24 01:23:22 +0000

    Revert "increase final vote to n=23 using remaining time budget"
    This reverts commit 35937129731a81422366f87a5a81be8f68e233f2.

--- train.py
+++ train.py
@@ -80,7 +80,7 @@ class ModeratorBot:
         return ""
 
     def finalize_intent(self):
-        self.t = self._vote_intent(n=23, temp=0.6)
+        self.t = self._vote_intent(n=17, temp=0.6)
         self.y = "benign" if _is_organic(self.t) else "malicious"
 
     def _generate_probe(self, critique: str) -> str:
