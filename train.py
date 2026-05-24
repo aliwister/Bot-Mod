bb5b9f03c25edeef36fbb5a02f639d996109d184
EC2 Default User <ec2-user@ip-172-31-0-242.us-east-2.compute.internal>
2026-05-24 05:06:39 +0000

    author instead of poster in opening probe for comment generality

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ class ModeratorBot:
             prompt = (
                 f"Community: {self.community}\n"
                 f"Content: {self.M}\n\n"
-                "Generate an opening question to understand the poster's motivation."
+                "Generate an opening question to understand the author's motivation."
             )
         else:
             prompt = (
