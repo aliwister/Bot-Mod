66245804961e5f494e77bd56f8efe2dd407afe62
EC2 Default User <ec2-user@ip-172-31-0-242.us-east-2.compute.internal>
2026-05-25 21:43:39 +0000

    seed: temp=0.62 (fine-grained between 0.6 and 0.65)

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ class ModeratorBot:
         self.community = community
         self.P = []
 
-        self.t = self._vote_intent(n=5, temp=0.6)
+        self.t = self._vote_intent(n=5, temp=0.62)
         self.y = "benign" if _is_organic(self.t) else "malicious"
         self.t0, self.y0 = self.t, self.y
 
